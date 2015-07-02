61cb499dde15462ed6edabd6fdfe41e3990545fb
Ryan Glassett <rglassett@gmail.com>
2015-07-01 17:07:14 -0700

    Add specs for w1d2 iteration exercises

---
+++ w1/w1d2/exercises/lib/iteration.rb
@@ -0,0 +1,46 @@
+def benchmark(count = 1, &prc)
+end
+
+def race(count = 1, procs)
+end
+
+def eval_block(*args, &prc)
+end
+
+def factors(num)
+end
+
+class Array
+  def bubble_sort!
+  end
+
+  def bubble_sort(&prc)
+  end
+end
+
+# You don't **strictly** need to implement this method, but we do suggest it.
+def substrings(string)
+end
+
+def subwords(word, dictionary)
+end
+
+def doubler(array)
+end
+
+class Array
+  def my_each(&prc)
+  end
+
+  def my_map(&prc)
+  end
+
+  def my_select(&prc)
+  end
+
+  def my_inject(&blk)
+  end
+end
+
+def concatenate(strings)
+end
