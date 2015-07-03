d97630149b13f4ee331081ba87299ce6379860e0
vveleva <vvveleva@gmail.com>
2015-07-03 14:24:34 -0700

    Add specs for factory methods and []

--- w3/w3d2/skeleton/lib/mastermind.rb
+++ w3/w3d2/skeleton/lib/mastermind.rb
@@ -1,70 +1,71 @@
-  class Code
-    PEGS = {
-      "B" => :blue,
-      "G" => :green,
-      "O" => :orange,
-      "P" => :purple,
-      "R" => :red,
-      "Y" => :yellow
-    }
+class Code
+  attr_reader :pegs
 
-    def self.parse(str)
-      # factory method
-      pegs = str.split("").map do |letter|
-        raise "parse error" unless PEGS.has_key?(letter)
+  PEGS = {
+    "B" => :blue,
+    "G" => :green,
+    "O" => :orange,
+    "P" => :purple,
+    "R" => :red,
+    "Y" => :yellow
+  }
 
-        PEGS[letter]
-      end
+  def self.parse(str)
+    # factory method
+    pegs = str.upcase.split("").map do |letter|
+      raise "parse error" unless PEGS.has_key?(letter)
 
-      Code.new(pegs)
+      PEGS[letter]
     end
 
-    def self.random
-      pegs = []
-      4.times { pegs << PEGS.values.sample }
+    Code.new(pegs)
+  end
 
-      Code.new(pegs)
+  def self.random
+    pegs = []
+    4.times { pegs << PEGS.values.sample }
+
+    Code.new(pegs)
+  end
+
+  def initialize(pegs)
+    @pegs = pegs
+  end
+
+  def [](i)
+    pegs[i]
+  end
+
+  def exact_matches(other_code)
+    exact_matches = 0
+    pegs.each_index do |i|
+      exact_matches += 1 if self[i] == other_code[i]
     end
 
-    def initialize(pegs)
-      @pegs = pegs
+    exact_matches
+  end
+
+  def near_matches(other_code)
+    other_color_counts = other_code.color_counts
+
+    near_matches = 0
+    self.color_counts.each do |color, count|
+      next unless other_color_counts.has_key?(color)
+
+      # Give credit for up to count near matches
+      near_matches += [count, other_color_counts[color]].min
     end
 
-    def [](i)
-      pegs[i]
-    end
+    near_matches - self.exact_matches(other_code)
+  end
 
-    def exact_matches(other_code)
-      exact_matches = 0
-      pegs.each_index do |i|
-        exact_matches += 1 if self[i] == other_code[i]
-      end
+  def ==(other_code)
+    return false unless other_code.is_a?(Code)
 
-      exact_matches
-    end
+    self.pegs == other_code.pegs
+  end
 
-    def near_matches(other_code)
-      other_color_counts = other_code.color_counts
-
-      near_matches = 0
-      self.color_counts.each do |color, count|
-        next unless other_color_counts.has_key?(color)
-
-        # Give credit for up to count near matches
-        near_matches += [count, other_color_counts[color]].min
-      end
-
-      near_matches - self.exact_matches(other_code)
-    end
-
-    def ==(other_code)
-      return false unless other_code.is_a?(Code)
-
-      self.pegs == other_code.pegs
-    end
-
-    protected
-    attr_reader :pegs
+  protected
 
     def color_counts
       color_counts = Hash.new(0)
@@ -75,52 +76,52 @@
 
       color_counts
     end
+end
+
+class Game
+  MAX_TURNS = 10
+
+  def initialize(secret_code = Code.random)
+    @secret_code = secret_code
   end
 
-  class Game
-    MAX_TURNS = 10
+  def play
+    MAX_TURNS.times do
+      guess = get_guess
 
-    def initialize(secret_code = Code.random)
-      @secret_code = secret_code
-    end
-
-    def play
-      MAX_TURNS.times do
-        guess = get_guess
-
-        if guess == @secret_code
-          puts "You're worth it!"
-          return
-        end
-
-        display_matches(guess)
+      if guess == @secret_code
+        puts "You're worth it!"
+        return
       end
 
-      puts "You aren't very good at this..."
+      display_matches(guess)
     end
 
-    private
+    puts "You aren't very good at this..."
+  end
 
-    def get_guess
-      puts "Guess the code:"
+  private
 
-      begin
-        Code.parse(gets.chomp)
-      rescue
-        puts "Error parsing code!"
-        retry
-      end
-    end
+  def get_guess
+    puts "Guess the code:"
 
-    def display_matches(guess)
-      exact_matches = @secret_code.exact_matches(guess)
-      near_matches = @secret_code.near_matches(guess)
-
-      puts "You got #{exact_matches} exact matches!"
-      puts "You got #{near_matches} near matches!"
+    begin
+      Code.parse(gets.chomp)
+    rescue
+      puts "Error parsing code!"
+      retry
     end
   end
+
+  def display_matches(guess)
+    exact_matches = @secret_code.exact_matches(guess)
+    near_matches = @secret_code.near_matches(guess)
+
+    puts "You got #{exact_matches} exact matches!"
+    puts "You got #{near_matches} near matches!"
+  end
+end
 
 if __FILE__ == $PROGRAM_NAME
-  Game.new.play
+Game.new.play
 end
--- w3/w3d2/skeleton/spec/mastermind_spec.rb
+++ w3/w3d2/skeleton/spec/mastermind_spec.rb
@@ -3,11 +3,87 @@ require 'mastermind'
 
 
 describe Code do
-  describe "::parse" do
-    # context "factory methods" do
-      it "should return an instance of Code" do
-        expect(Code.parse("BBBB")).to be_instance_of(Code)
-      end
-    # end
+  let(:uccode) { Code.parse("BBBB") }
+  let(:lccode) { Code.parse("bbbb") }
+
+  describe "::PEGS" do
+    it "should be a hash of key/value pairs" do
+      expect(Code::PEGS).to be_instance_of(Hash)
+    end
   end
+
+  describe "::parse" do
+    context "should return an instance of Code" do
+      # uccode = Code.parse("BBBB")
+      # lccode = Code.parse("bbbb")
+      it "it accepts upper case input" do
+        expect(uccode).to be_instance_of(Code)
+      end
+
+      it "it accepts lower case input" do
+        expect(lccode).to be_instance_of(Code)
+      end
+    end
+
+    it "raises error if input is not a valid peg color" do
+      expect { Code.parse("abcd") }.to raise_error
+    end
+  end
+
+  describe "::random" do
+    it "should return an instance of Code" do
+      expect(Code.random).to be_instance_of(Code)
+    end
+  end
+
+  describe "#initialize" do
+    it "expects pegs array to be passed in" do
+      expect { Code.new }.to raise_error(ArgumentError)
+    end
+
+    it "set it as an instance variable" do
+      pegs = ["b", "b", "b", "b"]
+      code = Code.new(pegs)
+
+      expect(code.pegs).to eq(pegs)
+    end
+  end
+
+  describe "#[]" do
+    it "should index into Code#pegs" do
+      pegs = [uccode[0], uccode[1], uccode[2], uccode[3]]
+      expect(pegs).to eq(uccode.pegs)
+    end
+  end
+
+  let(:code0) { Code.parse("oooo") }
+  let(:code1) { Code.parse("yyyb") }
+  let(:code2) { Code.parse("bboy") }
+
+  describe "#exact_matches" do
+    context "only counts matching color and position" do
+
+      it "should return 0 if no colors overlap" do
+        expect(uccode.exact_matches(code0)).to eq(0)
+      end
+
+      it "should return 0 if color match, but position doesn't" do
+        expect(code1.exact_matches(code2)).to eq(0)
+      end
+
+      it "should only count exact matches" do
+        expect(code0.exact_matches(code2)).to eq(1)
+      end
+    end
+  end
+
+end
+
+
+describe Game do
+  before do
+    $stdin = StringIO.new("BBBB\n")
+  end
+
+
 end
