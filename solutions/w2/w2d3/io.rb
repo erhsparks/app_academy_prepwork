05cd29504583479763d9c989fc4b91d6f3620362
Bibilidibipi <judy0wacker@gmail.com>
2015-07-14 10:56:26 -0700

    align guessing_game solution with specs

--- solutions/w2/w2d3/io.rb
+++ solutions/w2/w2d3/io.rb
@@ -3,18 +3,18 @@ def guessing_game
 
   guesses = 0
   while true
-    puts "Guess my secret!"
+    puts "Guess a number!"
     guess = Integer(gets.chomp)
     guesses += 1
 
     case guess <=> secret
     when -1
-      puts "Too low!"
+      puts "#{guess} is too low!"
     when 0
-      puts "My secret is found out!"
+      puts "You found the number #{guess}!"
       break
     when 1
-      puts "Too high!"
+      puts "#{guess} is too high!"
     end
   end
 
